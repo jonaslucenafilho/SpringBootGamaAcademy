7d94af9c42e2312dc65a396357e0afbc2cf6a647
Jonas Lucena <jonaslucena@outlook.com>
2021-10-28 07:28:46 -0300

    implementando na classe main para testa bd H2

--- apiRest/src/main/java/com/primeira/api/apiRest/ApiRestApplication.java
+++ apiRest/src/main/java/com/primeira/api/apiRest/ApiRestApplication.java
@@ -1,13 +1,30 @@
 package com.primeira.api.apiRest;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ApiRestApplication {
+import com.primeira.api.apiRest.model.Usuario;
+import com.primeira.api.apiRest.repository.UsuarioRepository;
 
+@SpringBootApplication
+public class ApiRestApplication implements CommandLineRunner {
+
+	@Autowired
+	UsuarioRepository usuarioRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ApiRestApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Usuario u1 = new Usuario(null, "Jonas Lucena", "joninhas");
+		Usuario u2 = new Usuario(null, "Marina Andrade", "nari");
+		
+		usuarioRepository.saveAll(Arrays.asList(u1, u2));	
+	}
 }
