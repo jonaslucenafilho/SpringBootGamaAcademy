c51ecdbff7724d36e1163c79dd8119b2ae179f85
Jonas Lucena <jonaslucena@outlook.com>
2021-10-29 20:28:52 -0300

    ajuste na classe Produto

--- apiRest/src/main/java/com/primeira/api/apiRest/model/Produto.java
+++ apiRest/src/main/java/com/primeira/api/apiRest/model/Produto.java
@@ -7,8 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Entity
@@ -23,7 +23,7 @@ public class Produto implements Serializable {
 	@Size(min = 3, max = 100)
 	private String nome;
 	
-	@NotBlank
+	@NotNull
 	private Double preco;
 
 	public Produto() {
